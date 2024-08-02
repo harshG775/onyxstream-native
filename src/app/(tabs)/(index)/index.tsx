423fce34a0e66eefa9406d538bcde46fdbcbe21b
Harsh <hgaur491@gmail.com>
2024-08-02 21:06:44 +0530

    fixing style a bit

--- src/app/(tabs)/(index)/index.tsx
+++ src/app/(tabs)/(index)/index.tsx
@@ -1,4 +1,6 @@
 import { Text, View } from "@/components/ui/Themed";
+import { colors, fontSize } from "@/constants/styles.constants";
+import { useThemeColor } from "@/hooks/useThemeColor";
 import { TrendingReleasingMedia } from "@/services/aniList/aniListTypes";
 import { useGetTrendingReleasing } from "@/services/aniList/queries.tanstack";
 // import { useGetRecentEpisodes } from "@/services/gogoanime/queries.tanstack";
@@ -16,8 +18,14 @@ export default function HomeScreen() {
                 <FlatList
                     horizontal={false}
                     data={data.media}
+                    // numColumns={2}
                     renderItem={({ item, index }) => (
-                        <AnimeListItem anime={item} key={index} />
+                        <>
+                            <AnimeListItem anime={item} key={index} />
+                        </>
+                    )}
+                    ItemSeparatorComponent={() => (
+                        <View style={{ padding: 4 }}></View>
                     )}
                 />
             )}
@@ -25,38 +33,62 @@ export default function HomeScreen() {
     );
 }
 function AnimeListItem({ anime }: { anime: TrendingReleasingMedia }) {
+    const primary = useThemeColor({}, "primary");
     return (
         <Link asChild href={`/anime/${anime.id}`}>
             <TouchableHighlight
                 activeOpacity={0.6}
-                underlayColor={"red"}
+                underlayColor={primary}
                 // onPress={() => alert("Pressed!")}
+                style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    padding: 2,
+                }}
             >
-                <View>
-                    <View>
-                        <Image
-                            style={styles.image}
-                            source={{ uri: anime.coverImage.large || "" }}
-                        />
+                <>
+                    <Image
+                        style={{
+                            flexShrink: 0,
+                            flexGrow: 0,
+                            width: 100,
+                            height: 100,
+                        }}
+                        source={{ uri: anime.coverImage.large || "" }}
+                    />
+
+                    <View style={{ flex: 1, paddingHorizontal: 4, gap: 4 }}>
+                        <Text>{anime.title.userPreferred}</Text>
+                        <Text
+                            style={{
+                                fontStyle: "italic",
+                            }}
+                        >
+                            {anime.averageScore
+                                ? `${(anime.averageScore * 0.1).toFixed(2)}/10`
+                                : "not rated"}
+                        </Text>
+                        <Text
+                            style={{
+                                fontWeight: "bold",
+                                fontStyle: "italic",
+                            }}
+                        >
+                            {anime.format}
+                        </Text>
+                        <View
+                            style={{
+                                display: "flex",
+                                flexDirection: "row",
+                            }}
+                        >
+                            {anime.genres.map((g) => (
+                                <Text key={g}>{`${g}, `}</Text>
+                            ))}
+                        </View>
                     </View>
-                    <Text>{anime.title.userPreferred}</Text>
-                </View>
+                </>
             </TouchableHighlight>
         </Link>
     );
 }
-// Image.resolveAssetSource(unknownTrackImage).uri
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    image: {
-        flex: 1,
-        width: "100%",
-        height: 100,
-        backgroundColor: "#0553",
-    },
-});
