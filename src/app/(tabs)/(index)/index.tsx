8c5c18c4adf244aa444567d4d5f75104dd42bc2e
Harsh <hgaur491@gmail.com>
2024-08-02 17:25:30 +0530

    ScrollView and flat list error fix

--- src/app/(tabs)/(index)/index.tsx
+++ src/app/(tabs)/(index)/index.tsx
@@ -1,41 +1,46 @@
-import { Text, View, ScrollView } from "@/components/ui/Themed";
+import { Text, View } from "@/components/ui/Themed";
 import { useGetRecentEpisodes } from "@/services/gogoanime/queries.tanstack";
+import { Link } from "expo-router";
 import { StyleSheet, Image, FlatList, TouchableHighlight } from "react-native";
 // import { Image } from "expo-image";
 export default function HomeScreen() {
     const { data, status, error, refetch } = useGetRecentEpisodes();
     if (status === "error") console.log(error);
     return (
-        <ScrollView>
+        <View>
             {status === "pending" && <Text>Loading...</Text>}
             {status === "error" && <Text>Something went wrong</Text>}
             {status === "success" && (
-                <ScrollView>
-                    <FlatList
-                        data={data.results}
-                        renderItem={({ item, index, separators }: any) => (
-                            <AnimeListItem anime={item} key={index} />
-                        )}
-                    />
-                </ScrollView>
+                <FlatList
+                    horizontal={false}
+                    data={data.results}
+                    renderItem={({ item, index }) => (
+                        <AnimeListItem anime={item} key={index} />
+                    )}
+                />
             )}
-        </ScrollView>
+        </View>
     );
 }
 function AnimeListItem({ anime }: any) {
     return (
-        <TouchableHighlight
-            activeOpacity={0.6}
-            underlayColor={"red"}
-            onPress={() => alert("Pressed!")}
-        >
-            <View>
+        <Link asChild href={`/(tabs)/anime`}>
+            <TouchableHighlight
+                activeOpacity={0.6}
+                underlayColor={"red"}
+                // onPress={() => alert("Pressed!")}
+            >
                 <View>
-                    <Image style={styles.image} source={{ uri: anime.image }} />
+                    <View>
+                        <Image
+                            style={styles.image}
+                            source={{ uri: anime.image }}
+                        />
+                    </View>
+                    <Text>{anime.title}</Text>
                 </View>
-                <Text>{anime.title}</Text>
-            </View>
-        </TouchableHighlight>
+            </TouchableHighlight>
+        </Link>
     );
 }
 // Image.resolveAssetSource(unknownTrackImage).uri
